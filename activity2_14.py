3d19abf17101a8087c9bbfb4057b8188b7fbb8cc
mahi-ihsom <devendrajoshi80@gmail.com>
2025-08-04 17:57:42 +0400

    Add files via upload

---
+++ activity2_14.py
@@ -0,0 +1,7 @@
+#weather forecast
+def weather_condition():
+    print("The weather is pleasant in spring.", spring)
+    print("The weather is same in autumn.", autumn)
+spring= "Autumn"
+autumn= spring
+weather_condition()
