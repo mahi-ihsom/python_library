1742b6bdd7c8ce88c34e5e5c0375d1841421cc7a
mahi-ihsom <devendrajoshi80@gmail.com>
2025-08-12 17:23:33 +0400

    Add files via upload

---
+++ activity1_20.py
@@ -0,0 +1,15 @@
+#list
+#write a program to perform the following operations on a list 
+#create an empty list, a list with elements, use multiply operator, and reversal list
+#1
+list1= []
+print()
+#2
+list2= [5,3,4,6,2]
+print(list2)
+#3
+list3= [1,2,3]*3
+print(list3)
+#4
+list4= [100, 200, 300, 400, 500]
+print(list4[::-1])
